fd9cc99f77ff8aa428f438ccb80772d9e075d793
gabrieldpf67 <silviociacell@gmail.com>
2026-05-18 12:22:53 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,1 +1,8 @@
-
+print ("escolha uma equação")
+a = input()
+b = int(input("escolha um numero   ="))
+c = int(input("escolha um numero   ="))
+if a == ("m"):
+    print(c + b)
+elif a == ("sub"):
+    print(c - b)
