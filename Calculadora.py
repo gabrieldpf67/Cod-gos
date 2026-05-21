b8dbf6d3cbf920b4f8038823fa96f0242a06bb81
gabrieldpf67 <silviociacell@gmail.com>
2026-05-21 09:48:18 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,8 +1,17 @@
 print ("escolha uma equação")
 a = input()
+if a == "raiz":
+    d = int(input("digite o valor da raiz  ="))
+    print(d ** 0.5)
 b = int(input("escolha um numero   ="))
 c = int(input("escolha um numero   ="))
 if a == ("m"):
     print(c + b)
 elif a == ("sub"):
     print(c - b)
+elif a == ("v"):
+    print(c * b)
+elif a == ("res"):
+    print(c % b)
+elif a == ("div"):
+    print(c / b)
