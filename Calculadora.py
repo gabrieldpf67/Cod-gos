bc1c250da02adb7903da09a25080d1fa6223df93
gabrieldpf67 <silviociacell@gmail.com>
2026-05-25 12:25:48 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,17 +1,21 @@
-print ("escolha uma equação")
-a = input()
-if a == "raiz":
-    d = int(input("digite o valor da raiz  ="))
-    print(d ** 0.5)
-b = int(input("escolha um numero   ="))
-c = int(input("escolha um numero   ="))
-if a == ("m"):
-    print(c + b)
-elif a == ("sub"):
-    print(c - b)
-elif a == ("v"):
-    print(c * b)
-elif a == ("res"):
-    print(c % b)
-elif a == ("div"):
-    print(c / b)
+while 0 == 0:
+    print ("1 = adição \n2 = subtração \n3 = multiplicação \n4 = resto\n5 = divisão\n6 = poténcia\n7 = raiz\n  [escolha uma operação]")
+    a = input()
+    if a == "7":
+        d = int(input("digite o valor da raiz  ="))
+        print(d ** 0.5)
+        continue
+    b = int(input("escolha um numero   ="))
+    c = int(input("escolha um numero   ="))
+    if a == ("1"):
+        print(c + b)
+    elif a == ("2"):
+        print(c - b)
+    elif a == ("3"):
+        print(c * b)
+    elif a == ("4"):
+        print(c % b)
+    elif a == ("5"):
+        print(c / b)
+    elif a == ("6"):
+        print(c ** b)
